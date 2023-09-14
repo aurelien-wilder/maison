9c0aa564498c5569ed2377a599ce58636c0dfc1c
Aurélien Dazy <137416384+aurelien-wilder@users.noreply.github.com>
2023-09-15 00:45:49 +0200

    (!)[sh] Update install.sh

--- install.sh
+++ install.sh
@@ -3,25 +3,26 @@
 # URL du fichier compressé contenant le labyrinthe
 Maison_URL="https://github.com/aurelien-wilder/maison/raw/main/maison.tar.gz"
 
-# Télécharger le fichier compressé et le décompresser
-wget -O maison.tar.gz "$Maison_URL" 
+# Télécharger le fichier compressé
+wget -O maison.tar.gz "$Maison_URL"
 
-sleep 5
-
-tar -xzvf maison.tar.gz
-
-# Afficher un message de félicitations
-echo "La construction de la maison est terminée ! Le jeu peut commencer."
-
-# Affichez les règles du jeu
-echo "Règles du jeu :"
-echo "Vous êtes dans une magnifique maison, composée de 4 pièces, à la recherche d'un trésor légendaire dont votre formateur à la Wild vous a parlé."
-echo "Utilisez les commandes 'ls' et 'cd' pour vous déplacer à travers celle-ci et trouver un coffre à trésor."
-echo "Utilisez 'cat' pour afficher le contenu des différents éléments que vous trouvez au fur et à mesure de votre exploration."
-echo "Une fois en possession du contenu du coffre, venez compléter la quête qui vous a mener ici :)."
-echo "Bonne chasse !"
-
-# Indiquez comment commencer
-echo "Pour commencer, exécutez :"
-echo "cd Maison"
-echo "ls"
+# Vérifier si le téléchargement a réussi
+if [ $? -eq 0 ]; then
+    # Extraire le contenu si le téléchargement a réussi
+    tar -xzvf maison.tar.gz
+    # Afficher un message de félicitations
+    echo "La construction de la maison est terminée ! Le jeu peut commencer."
+    # Afficher les règles du jeu et les instructions
+    echo "Règles du jeu :"
+    echo "Vous êtes dans une magnifique maison, composée de 4 pièces, à la recherche d'un trésor légendaire dont votre formateur à la Wild vous a parlé."
+    echo "Utilisez les commandes 'ls' et 'cd' pour vous déplacer à travers celle-ci et trouver un coffre à trésor."
+    echo "Utilisez 'cat' pour afficher le contenu des différents éléments que vous trouvez au fur et à mesure de votre exploration."
+    echo "Une fois en possession du contenu du coffre, venez compléter la quête qui vous a mené ici :)."
+    echo "Bonne chasse !"
+    # Indiquer comment commencer
+    echo "Pour commencer, exécutez :"
+    echo "cd Maison"
+    echo "ls"
+else
+    echo "Échec du téléchargement du fichier."
+fi
